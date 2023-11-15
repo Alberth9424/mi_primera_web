a0ef72b23158d7fb892180b7ca1f62d8fff856f7
Alberth9424 <carlos_alberto_hoyos@hotmail.com>
2023-11-14 19:37:52 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//alert("Hola este es mi Javascript");
+
+let nombre = "Carlos ";
+
+var nombre1 = "Alberth";
+
+const nombre2 = "Marlon";
+
+console.log(nombre);
+console.log(nombre1);
+console.log(nombre2);
